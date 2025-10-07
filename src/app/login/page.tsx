0620078e601dad4e9507b520afacd54896008a7f
Yannick220291 <jeandedieuhasinirina82@example.com>
2025-10-07 14:36:20 +0300

    style: changement du couleur d'icone yeux sur la page de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -53,7 +53,8 @@ const LoginPage = () => {
               onClick={() => setShowPassword(!showPassword)}
               className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
             >
-              {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+              {showPassword ? <EyeOff size={20} className="text-purple-600 hover:text-purple-700 transition"/> :
+                              <Eye size={20} className="text-purple-600 hover:text-purple-700 transition"/>}
             </button>
           </div>
 
